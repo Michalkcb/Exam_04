f67848fb62993a1cbf9af04a37d9772daf1181aa
Michael <126069497+Michalkcb@users.noreply.github.com>
2025-02-04 19:05:26 +0100

    main done

--- mshell.c
+++ mshell.c
@@ -1,6 +1,25 @@
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int exec(char **av, int i, char **envp)
+{
+	
+}
+
 int main(int ac, char **av, char **envp)
 {
 	(void)ac;
 	int i = 0, status = 0;
-	
+	while (av[i])
+	{
+		av += i + 1;
+		i = 0;
+		while (av[i] && strcmp(av[i],"|") && strcmp(av[i], ";"))
+			i++;
+		if (i)
+			status = exec(av, i, envp);
+	}
+	return status;
 }
